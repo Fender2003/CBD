077aff32e999ed73a4256dc257a7b9d1d52c4d03
21bce300 <dhruvthakkar2511@gmail.com>
2026-02-23 13:25:31 +0100

    lobby

--- app/db/session.py
+++ app/db/session.py
@@ -1,6 +1,7 @@
 # DB connection
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import QueuePool
 
 from app.core.config import settings  # we'll define this in config.py
 
@@ -8,7 +9,23 @@ from app.core.config import settings  # we'll define this in config.py
 # "postgresql://username:password@localhost:5432/dbname"
 SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    poolclass=QueuePool,
+    pool_pre_ping=True,
+    pool_recycle=300,
+    pool_size=5,
+    max_overflow=10,
+    pool_timeout=30,
+    connect_args={
+        "connect_timeout": 10,
+        "sslmode": "require",
+        "keepalives": 1,
+        "keepalives_idle": 30,
+        "keepalives_interval": 10,
+        "keepalives_count": 5,
+    },
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
